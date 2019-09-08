864a91f4e30b93d46676ae0b45183c13b542b7d9
Steve Gordon <sgordon@hotmail.co.uk>
2019-09-08 19:17:57 +0100

    initial

---
+++ DotMemoryDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using JetBrains.Profiler.Api;
+using System;
+
+namespace DotMemoryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryProfiler.CollectAllocations(true);
+
+            MemoryProfiler.GetSnapshot();
+
+            var arrayOfValues = new[] { 10, 20, 30, 40, 50 };
+
+            var thing = new SomeClass(arrayOfValues, "FiveValues");
+
+            var total = thing.GetTotal();
+
+            MemoryProfiler.GetSnapshot();
+        }
+    }
+
+    public class SomeClass
+    {
+        private readonly int[] values;
+
+        public SomeClass(int[] values, string name)
+        {
+            this.values = values;
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public int GetTotal()
+        {
+            var total = 0;
+
+            foreach (var value in values)
+            {
+                total += value;
+            }
+
+            return total;
+        }
+    }
+}
